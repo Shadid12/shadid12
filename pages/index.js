72b89e4b9a6c98509b75e1243e28f944dc2a19e1
shadid12 <shadid.haque@avanade.com>
2020-03-28 12:11:22 -0400

    first commit done

--- pages/index.js
+++ pages/index.js
@@ -1,203 +1,265 @@
 import Head from 'next/head'
+import '../styles/style.sass'
+import { useState } from 'react'
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+const products = [
+  [
+    {
+      id: '1',
+      name: 'Macchiato',
+      price: 3.50,
+      imgUrl: 'https://media3.s-nbcnews.com/j/newscms/2019_33/2203981/171026-better-coffee-boost-se-329p_67dfb6820f7d3898b5486975903c2e51.fit-760w.jpg'
+    },
+    {
+      id: '2',
+      name: 'Esspresso',
+      price: 2.50,
+      imgUrl: 'https://coffeebros.com/wp-content/uploads/2019/04/Espresso-crema-Here%E2%80%99s-why-it%E2%80%99s-important.png'
+    },
+    {
+      id: '3',
+      name: 'Americano',
+      price: 3.95,
+      imgUrl: 'https://www.craftcoffeeguru.com/wp-content/uploads/2019/02/americano-9.jpg?x84304'
+    },
+    {
+      id: '4',
+      name: 'Hot Chocolate',
+      price: 5.50,
+      imgUrl: 'https://www.thespruceeats.com/thmb/7-cXy5t3G8u4G0546N7JfJC3lwk=/1660x1245/smart/filters:no_upscale()/homemade-hot-chocolate-15-56a8bdfe5f9b58b7d0f4bbe2.jpg'
+    }
+  ],
+  [
+    {
+      id: '5',
+      name: 'Americano',
+      price: 3.95,
+      imgUrl: 'https://www.craftcoffeeguru.com/wp-content/uploads/2019/02/americano-9.jpg?x84304'
+    },
+    {
+      id: '6',
+      name: 'Hot Chocolate',
+      price: 5.50,
+      imgUrl: 'https://www.thespruceeats.com/thmb/7-cXy5t3G8u4G0546N7JfJC3lwk=/1660x1245/smart/filters:no_upscale()/homemade-hot-chocolate-15-56a8bdfe5f9b58b7d0f4bbe2.jpg'
+    },
+    {
+      id: '7',
+      name: 'Americano',
+      price: 3.95,
+      imgUrl: 'https://www.craftcoffeeguru.com/wp-content/uploads/2019/02/americano-9.jpg?x84304'
+    },
+    {
+      id: '8',
+      name: 'Hot Chocolate',
+      price: 5.50,
+      imgUrl: 'https://www.thespruceeats.com/thmb/7-cXy5t3G8u4G0546N7JfJC3lwk=/1660x1245/smart/filters:no_upscale()/homemade-hot-chocolate-15-56a8bdfe5f9b58b7d0f4bbe2.jpg'
+    }
+  ]
+]
 
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
+const Home = () => {
 
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+  const [cart, setCart] = useState([])
+  const [showModal, setModalVis] = useState(false)
+  const [signinModal, setsigninModal] = useState(false)
+  const [signupModal, setsignupModal] = useState(false)
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
+  const addToCart = (item) => {
+    setCart([...cart, item])
+  }
 
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
+  const openModal = () => {
+    setModalVis(!showModal)
+  }
 
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
+  const closeSignin = () => {
+    setsigninModal(!signinModal)
+  }
+
+  const openSignup = () => {
+    setsigninModal(!signinModal)
+    setsignupModal(!signupModal)
+  }
+
+  const removeItem = (a) => {
+    var newCart = JSON.parse(JSON.stringify(cart))
+    let i = newCart.findIndex((item) => item.id === a.id)
+    newCart.splice(i, 1)
+    setCart(newCart)
+  }
+
+  const login = () => {
+
+  }
+
+  const signup = () => {
+
+  }
+  
+  return (
+    <div className="container">
+      <Head>
+        <title>Hobby Coffee</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <nav className="level">
+      <div className="level-left">
+        <div className="level-item">
+          <p className="subtitle is-5">
+            <strong>Hobby</strong> Coffee
           </p>
-        </a>
+        </div>
+        <div className="level-item">
+          <div className="field has-addons">
+            <p className="control">
+              <button className="button" onClick={closeSignin}>
+                Signin
+              </button>
+            </p>
+          </div>
+        </div>
       </div>
-    </main>
+      
+      <div className="level-right">
+        <div className="level-item">
+          <button className="subtitle is-5" onClick={openModal}>
+            <strong> {cart.length} 🛒</strong>
+          </button>
+          <button className="subtitle is-5">
+            <strong>Pay Now 💰</strong>
+          </button>
+        </div>
+      </div>
+      </nav>
+      
 
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
+        {products.map((product) => 
+          (
+            <div className="columns">
+              {product.map(item => (
+                <div className="column">
+                  <button className="card-btn" onClick={() => {addToCart(item)}}>
+                    <figure className="image is-3by2">
+                      <img src={item.imgUrl} />
+                    </figure>
+                    <span>{item.name}</span>
+                    <br />
+                    <strong>🏷️${item.price}</strong>
+                  </button>
+                </div>
+              ))}
+            </div>
+          )  
+        )}
 
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
+        {
+          showModal ? (
+            <div className="modal is-active">
+              <div className="modal-background"></div>
+              <div className="modal-card">
+                <header className="modal-card-head">
+                  <p className="modal-card-title">Items on Cart</p>
+                  <button className="delete" aria-label="close" onClick={openModal}></button>
+                </header>
+                <section className="modal-card-body">
+                  <div >
+                    {cart.map(item => (
+                      <li>
+                        {item.name}  {item.price}
+                        <button className="cart-btn" onClick={() => {
+                          removeItem(item)
+                        }}>🗑️</button>
+                      </li>
+                    ))}
+                    <hr />
+                    <span>Totoal: 0</span>
+                  </div>
+                </section>
+                <footer className="modal-card-foot">
+                  <button className="button is-success" onClick={openModal}>Save changes</button>
+                </footer>
+              </div>
+            </div>
+          ) : null
         }
-      }
-    `}</style>
 
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
+        {
+          signinModal ? (
+            <div className="modal is-active">
+              <div class="modal-background"></div>
+              <div class="modal-card">
+                <header class="modal-card-head">
+                  <p class="modal-card-title">Signin</p>
+                  <button class="delete" aria-label="close" onClick={closeSignin}></button>
+                </header>
+                <section class="modal-card-body">
+                  <div >
+                    <div class="field">
+                      <div class="control">
+                        <input class="input is-primary" type="text" placeholder="Email" />
+                      </div>
+                    </div>
+                    <div class="field">
+                      <div class="control">
+                        <input class="input is-primary" type="password" placeholder="Password" />
+                      </div>
+                    </div>
+                    <div class="field">
+                      <div class="control">
+                        <button class="button is-success" onClick={login}>Login</button>
+                        <button class="button btn-signup" onClick={openSignup}>Register Now!!</button>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
+          ) : null
+        }
 
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-)
+        {
+          signupModal ? (
+            <div className="modal is-active">
+              <div class="modal-background"></div>
+              <div class="modal-card">
+                <header class="modal-card-head">
+                  <p class="modal-card-title">Register Now!!</p>
+                  <button class="delete" aria-label="close" onClick={openSignup}></button>
+                </header>
+                <section class="modal-card-body">
+                  <div >
+                    <div class="field">
+                      <div class="control">
+                        <input class="input is-primary" type="text" placeholder="Email" />
+                      </div>
+                    </div>
+                    <div class="field">
+                      <div class="control">
+                        <input class="input is-primary" type="password" placeholder="Password" />
+                      </div>
+                    </div>
+                    <div class="field">
+                      <div class="control">
+                        <input class="input is-primary" type="password" placeholder="Re Enter Password" />
+                      </div>
+                    </div>
+                    <div class="field">
+                      <div class="control">
+                        <button class="button is-success" onClick={signup}>Register</button>
+                        <button class="button btn-signup" onClick={openSignup}>Login</button>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
+          ) : null
+        }
+    
+      </div>
+  )
+
+}
 
 export default Home
